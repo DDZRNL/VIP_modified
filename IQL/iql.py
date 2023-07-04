e600ca715cabcd935c96bca586d1ac987f8cf360
Haotian Wang <46818795+DDZRNL@users.noreply.github.com>
2023-07-03 20:45:07 -0500

    Add files via upload

---
+++ IQL/iql.py
@@ -0,0 +1,76 @@
+import copy
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim.lr_scheduler import CosineAnnealingLR
+
+from .util import DEFAULT_DEVICE, compute_batched, update_exponential_moving_average
+
+
+EXP_ADV_MAX = 100.
+
+# in Expectile regression part, tau ∈ (0,1). 
+def asymmetric_l2_loss(u, tau):
+    return torch.mean(torch.abs(tau - (u < 0).float()) * u**2)
+
+
+class ImplicitQLearning(nn.Module):
+    def __init__(self, qf, vf, policy, optimizer_factory, max_steps,
+                 tau, beta, discount=0.99, alpha=0.005):
+        super().__init__()
+        self.qf = qf.to(DEFAULT_DEVICE)
+        self.q_target = copy.deepcopy(qf).requires_grad_(False).to(DEFAULT_DEVICE)
+        self.vf = vf.to(DEFAULT_DEVICE)
+        self.policy = policy.to(DEFAULT_DEVICE)
+        self.v_optimizer = optimizer_factory(self.vf.parameters())  # vf, qf, policy are neural network
+        self.q_optimizer = optimizer_factory(self.qf.parameters())
+        self.policy_optimizer = optimizer_factory(self.policy.parameters())
+        self.policy_lr_schedule = CosineAnnealingLR(self.policy_optimizer, max_steps)
+        self.tau = tau
+        self.beta = beta
+        self.discount = discount
+        self.alpha = alpha
+
+    def update(self, observations, next_observations, rewards, terminals):
+        with torch.no_grad():
+            target_q = self.q_target(observations, next_observations)
+            next_v = self.vf(next_observations)
+
+        # v, next_v = compute_batched(self.vf, [observations, next_observations])
+
+        # Update value function     # function (5)
+        v = self.vf(observations)
+        # Learning the value function with expectile regression
+        adv = target_q - v
+        v_loss = asymmetric_l2_loss(adv, self.tau)
+        self.v_optimizer.zero_grad(set_to_none=True)
+        v_loss.backward()
+        self.v_optimizer.step()
+
+        # Update Q function     # function (6)
+        targets = rewards + (1. - terminals.float()) * self.discount * next_v.detach()
+        qs = self.qf.both(observations, next_observations)
+        q_loss = sum(F.mse_loss(q, targets) for q in qs) / len(qs)
+        self.q_optimizer.zero_grad(set_to_none=True)
+        q_loss.backward()
+        self.q_optimizer.step()
+
+        # Update target Q network
+        update_exponential_moving_average(self.q_target, self.qf, self.alpha)
+
+        # Update policy     # function (7)
+        exp_adv = torch.exp(self.beta * adv.detach()).clamp(max=EXP_ADV_MAX)
+        policy_out = self.policy(observations)
+        if isinstance(policy_out, torch.distributions.Distribution):
+            bc_losses = -policy_out.log_prob(next_observations)
+        elif torch.is_tensor(policy_out):
+            assert policy_out.shape == next_observations.shape
+            bc_losses = torch.sum((policy_out - next_observations)**2, dim=1)
+        else:
+            raise NotImplementedError
+        policy_loss = torch.mean(exp_adv * bc_losses)
+        self.policy_optimizer.zero_grad(set_to_none=True)
+        policy_loss.backward()
+        self.policy_optimizer.step()
+        self.policy_lr_schedule.step()
