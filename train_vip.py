596401e198e1e17af7fa8080f6293fa83b59dfa8
Haotian Wang <46818795+DDZRNL@users.noreply.github.com>
2024-03-22 23:44:57 -0500

    Update train_vip.py

--- train_vip.py
+++ train_vip.py
@@ -93,6 +93,7 @@ class Workspace:
         q_hidden_dim = self.cfg.q_hidden_dim
         
         target_model = copy.deepcopy(self.model).to(DEFAULT_DEVICE)
+        encoder_q = copy.deepcopy(self.model).to(DEFAULT_DEVICE)
         optimizer_factory=lambda params: torch.optim.Adam(params, lr=1e-4)
         trainer = Trainer(target_model, eval_freq, obs_dim, optimizer_factory, v_hidden_dim, q_hidden_dim)
 
@@ -104,7 +105,7 @@ class Workspace:
             batch_f, batch_rewards = next(self.train_loader)
             t1 = time.time()
             # print('train_vip 程序运行时间:%s毫秒' % ((t1 - t0)*1000))
-            metrics, st = trainer.update(self.model, (batch_f.cuda(), batch_rewards), self.global_step)
+            metrics, st = trainer.update(self.model, encoder_q, (batch_f.cuda(), batch_rewards), self.global_step)
             t2 = time.time()
             self.logger.log_metrics(metrics, self.global_frame, ty='train')
 
@@ -115,7 +116,7 @@ class Workspace:
             if eval_every_step(self.global_step):
                 with torch.no_grad():
                     batch_f, batch_rewards = next(self.val_loader)
-                    metrics, st = trainer.update(self.model, (batch_f.cuda(), batch_rewards), self.global_step, eval=True)
+                    metrics, st = trainer.update(self.model, encoder_q,(batch_f.cuda(), batch_rewards), self.global_step, eval=True)
                     self.logger.log_metrics(metrics, self.global_frame, ty='eval')
                     print("EVAL", self.global_step, metrics)
 
