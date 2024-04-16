8baaefeed833b04e6c3eaa9f9189d55ea7b0fa3d
Haotian Wang <46818795+DDZRNL@users.noreply.github.com>
2024-04-16 17:28:32 -0500

    Update data_loaders.py

--- utils/data_loaders.py
+++ utils/data_loaders.py
@@ -28,15 +28,6 @@ import json
 import random
 
 
-def get_ind(vid, index, ds="ego4d"):
-    if ds == "ego4d":
-        return torchvision.io.read_image(f"{vid}{index:06}.jpg")
-    else:
-        try:
-            return torchvision.io.read_image(f"{vid}/{index}.jpg")
-        except: 
-            return torchvision.io.read_image(f"{vid}/{index}.png")
-
 ## Data Loader for VIP
 class VIPBuffer(IterableDataset):
     def __init__(self, datasource='ego4d', datapath=None, num_workers=10, doaug = "none"):
@@ -45,45 +36,21 @@ class VIPBuffer(IterableDataset):
         self.datapath = datapath
         assert(datapath is not None)
         self.doaug = doaug
+        # self.data_paths = np.load(self.datapath, allow_pickle=True)
+        with open(self.datapath, 'rb') as f:
+            self.data_paths = pickle.load(f)
         
-        # Augmentations
-        self.preprocess = torch.nn.Sequential(
-                        transforms.Resize(256),
-                        transforms.CenterCrop(224)
-                )
-        if doaug in ["rc", "rctraj"]:
-            self.aug = torch.nn.Sequential(
-                transforms.RandomResizedCrop(224, scale = (0.2, 1.0)),
-            )
-        else:
-            self.aug = lambda a : a
-
         # Load Data
-        if "ego4d" == self.datasource:
-            print("Ego4D")
-            self.manifest = pd.read_csv(f"{self.datapath}/manifest.csv")
-            print(self.manifest)
-            self.ego4dlen = len(self.manifest)
 
     def _sample(self):
+
         # Sample a video from datasource
-        if self.datasource == 'ego4d':
-            vidid = np.random.randint(0, self.ego4dlen)
-            m = self.manifest.iloc[vidid]
-            vidlen = m["len"]
-            vid = m["path"]
-        else: 
-            video_paths = glob.glob(f"{self.datapath}/[0-9]*")
-            num_vid = len(video_paths)
-            #print("!!! video_path: ", video_paths)
+        num_vid = len(self.data_paths)
 
-            video_id = np.random.randint(0, int(num_vid)) 
-            vid = f"{video_paths[video_id]}"
+        video_id = np.random.randint(0, int(num_vid))
+        vid = self.data_paths[video_id]
 
-            # Video frames must be .png or .jpg
-            vidlen = len(glob.glob(f'{vid}/*.png'))
-            if vidlen == 0:
-                vidlen = len(glob.glob(f'{vid}/*.jpg'))
+        vidlen = len(vid)
 
         # Sample (o_t, o_k, o_k+1, o_T) for VIP training
         start_ind = np.random.randint(0, vidlen-2)
@@ -95,30 +62,14 @@ class VIPBuffer(IterableDataset):
         # Self-supervised reward (this is always -1)
         reward = float(s0_ind_vip == end_ind) - 1
 
-        if self.doaug == "rctraj":
-            ### Encode each image in the video at once the same way
-            im0 = get_ind(vid, start_ind, self.datasource) 
-            img = get_ind(vid, end_ind, self.datasource)
-            imts0_vip = get_ind(vid, s0_ind_vip, self.datasource)
-            imts1_vip = get_ind(vid, s1_ind_vip, self.datasource)
-            
-            allims = torch.stack([im0, img, imts0_vip, imts1_vip], 0)
-            allims_aug = self.aug(allims / 255.0) * 255.0
+        ### Encode each image individually
+        im0 = vid[start_ind]
+        img = vid[end_ind]
+        imts0_vip = vid[s0_ind_vip]
+        imts1_vip = vid[s1_ind_vip]
 
-            im0 = allims_aug[0]
-            img = allims_aug[1]
-            imts0_vip = allims_aug[2]
-            imts1_vip = allims_aug[3]
-
-        else:
-            ### Encode each image individually
-            im0 = self.aug(get_ind(vid, start_ind, self.datasource) / 255.0) * 255.0
-            img = self.aug(get_ind(vid, end_ind, self.datasource) / 255.0) * 255.0
-            imts0_vip = self.aug(get_ind(vid, s0_ind_vip, self.datasource) / 255.0) * 255.0
-            imts1_vip = self.aug(get_ind(vid, s1_ind_vip, self.datasource) / 255.0) * 255.0
-
-        im = torch.stack([im0, img, imts0_vip, imts1_vip])
-        im = self.preprocess(im)
+        # print("The data loader:")
+        im = torch.stack([torch.tensor(im0), torch.tensor(img), torch.tensor(imts0_vip), torch.tensor(imts1_vip)])
         return (im, reward)
 
     def __iter__(self):
